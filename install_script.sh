9f098196dc5eb855fbd88bc0b3740550210cf74f
florian <florian.sananes@laposte.net>
2021-01-18 15:59:07 +0100

    Fixed data path

--- install_script.sh
+++ install_script.sh
@@ -4,6 +4,7 @@
 wget -O opennn.tar.gz https://github.com/Artelnics/opennn/archive/v4.0.tar.gz
 tar -xvf opennn.tar.gz
 cd opennn-4.0
+find examples -name \*.cpp -exec sed -i "s/..\/data/data/g" {} \; # fixing data path
 cmake .
 cmake -DCMAKE_TYPE_BUILD=Release .
 make
@@ -17,4 +18,3 @@ sudo apt install python3-pip
 sudo pip3 install Theano -t ~/pip_packages/
 sudo pip3 install pydot -t ~/pip_packages/
 export PYTHONPATH=~/pip_packages/
-
