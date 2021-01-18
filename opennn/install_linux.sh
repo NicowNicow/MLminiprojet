b2e053a4841d2bdd7ea32b89dc5837453af779c7
florian <florian.sananes@laposte.net>
2021-01-18 11:48:24 +0100

    Making proper install scripts

--- opennn/install_linux.sh
+++ opennn/install_linux.sh
@@ -1,4 +1,3 @@
-```sh
 cd
 git clone https://github.com/Artelnics/opennn.git
 cd opennn
@@ -6,4 +5,3 @@ sudo apt install make cmake
 cmake .
 cmake -DCMAKE_TYPE_BUILD=Release .
 make
-```
--- theano/install_linux.sh
+++ theano/install_linux.sh
@@ -1,4 +1,3 @@
-```sh
 cd
 mkdir pip_packages
 sudo apt install python3
@@ -6,4 +5,3 @@ sudo apt install python3-pip
 sudo pip3 install Theano -t ~/pip_packages/
 sudo pip3 install pydot -t ~/pip_packages/
 export PYTHONPATH=~/pip_packages/
-```
